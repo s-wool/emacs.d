42b5461af9c546f30e700644b6011e27ff9153d4
s-wool <s_wool@mac.com>
2014-08-13 14:00:18 +0900

    scratchを保存

--- init.el
+++ init.el
@@ -172,3 +172,35 @@
 (when (require 'redo+ nil t)
   (global-set-key (kbd "C-c '") 'redo))
 (setq undo-no-redo t)
+
+;;; auto save and restore scratch buffer
+(defun save-scratch-data ()
+  (let ((str (progn
+               (set-buffer (get-buffer "*scratch*"))
+               (buffer-substring-no-properties
+                (point-min) (point-max))))
+        (file "~/.scratch"))
+    (if (get-file-buffer (expand-file-name file))
+        (setq buf (get-file-buffer (expand-file-name file)))
+      (setq buf (find-file-noselect file)))
+    (set-buffer buf)
+    (erase-buffer)
+    (insert str)
+    (save-buffer)
+    (kill-buffer buf)))
+
+(defadvice save-buffers-kill-emacs
+  (before save-scratch-buffer activate)
+  (save-scratch-data))
+
+(defun read-scratch-data ()
+  (let ((file "~/.scratch"))
+    (when (file-exists-p file)
+      (set-buffer (get-buffer "*scratch*"))
+      (erase-buffer)
+      (insert-file-contents file))
+    ))
+
+(setq recentf-auto-cleanup 'never)
+
+(read-scratch-data)    ;; ←これは初期設定ファイルの最後に追加
